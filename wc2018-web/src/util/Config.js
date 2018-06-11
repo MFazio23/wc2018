e73b67ddf1f9cca5d7b877c09f26dd4b933d8cdf
Michael Fazio <mfazio@skylinetechnologies.com>
2018-06-10 23:38:53 -0500

    Switch back to prod URL in web

--- wc2018-web/src/util/Config.js
+++ wc2018-web/src/util/Config.js
@@ -4,6 +4,6 @@ const firebaseEnv = "test";
 export const Config = {
     firebaseEnv: firebaseEnv,
     firebaseBaseUrl: `https://wc2018-2bad0.firebaseio.com/${firebaseEnv}`,
-    //apiBaseUrl: "https://wc2018-2bad0.firebaseio.com/"
-    apiBaseUrl: "http://localhost:8080/"
+    apiBaseUrl: "https://wc2018-api.faziodev.org/"
+    //apiBaseUrl: "http://localhost:8080/"
 };
