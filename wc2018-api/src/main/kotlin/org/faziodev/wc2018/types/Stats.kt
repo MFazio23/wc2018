ac6e6a72b25de6c8c77db264c63976d41bc88655
Michael Fazio <mfazio@skylinetechnologies.com>
2018-06-25 11:09:51 -0500

    Add 'eliminated' flag to stats

--- wc2018-api/src/main/kotlin/org/faziodev/wc2018/types/Stats.kt
+++ wc2018-api/src/main/kotlin/org/faziodev/wc2018/types/Stats.kt
@@ -1,12 +1,13 @@
 package org.faziodev.wc2018.types
 
-data class Stats(val cs: Int, val d: Int, val g: Int, val w: Int) {
+data class Stats(val cs: Int, val d: Int, val g: Int, val w: Int, val eliminated: Boolean = false) {
     operator fun plus(other: Stats): Stats {
         return Stats(
             this.cs + other.cs,
             this.d + other.d,
             this.g + other.g,
-            this.w + other.w
+            this.w + other.w,
+            this.eliminated || other.eliminated
         )
     }
 
